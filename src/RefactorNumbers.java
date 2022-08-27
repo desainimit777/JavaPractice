f4d46b31734b8be48780b28dc3528cc1d989c32e
jyotip <jyotip@splunk.com>
2022-08-27 17:36:38 +0530

    first commit

---
+++ src/RefactorNumbers.java
@@ -0,0 +1,29 @@
+class RefactorNumbers {
+
+    public static void main(String[] args) {
+        int arr[]={9, 4, -2, -1, 5, 0, -5, -3, 2};
+        int n=9;
+        RefactorNumbers.rearrange(arr,n);
+    }
+
+    public static void rearrange(int arr[],int n) {
+        int neg=0;
+        int pos=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<0)
+                neg++;
+            else
+                pos++;
+        }
+
+        int posArr[] =new int[pos];
+        int negArr[] =new int[neg];
+
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Hi");
+        }
+    }
+}
+
